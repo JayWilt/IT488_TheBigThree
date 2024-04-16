518b196c1a5a13b420d8d96ea536c08ab48a77da
JayWilt <155564660+JayWilt@users.noreply.github.com>
2024-04-16 12:25:32 -0500

    Add files via upload

--- IT488_TheBigThreeSupport.sql
+++ IT488_TheBigThreeSupport.sql
@@ -1,3 +1,23 @@
 
 CREATE DATABASE TheBigThreeSupport;
 
+USE TheBigThreeSupport;
+CREATE TABLE Service_Ticket (
+Ticket_Number int NOT NULL IDENTITY,
+Ticket_Date DATE,
+Customer_First_Name VARCHAR(25),
+Customer_Last_Name VARCHAR(35),
+Customer_Email VARCHAR(45),
+Customer_Phone CHAR(9),
+Service_Tech_ID VARCHAR(20),
+Ticket_Status VARCHAR(20),
+Ticket_Description VARCHAR(500)
+);
+
+USE TheBigThreeSupport;
+CREATE TABLE Service_Tech(
+Service_Tech_ID VARCHAR(10),
+Service_Tech_First_Name VARCHAR(25),
+Service_Tech_Last_Name VARCHAR(35),
+Service_Tech_Department VARCHAR(20)
+);
