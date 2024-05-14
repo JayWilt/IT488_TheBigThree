790f5b083efae77d040dc591c7d2c49179234331
JayWilt <155564660+JayWilt@users.noreply.github.com>
2024-05-14 13:37:40 -0500

    Add files via upload

---
+++ Form4.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace IT488_TheBigThreeServiceTicket
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            linkLabel1 = new LinkLabel();
+            linkLabel2 = new LinkLabel();
+            linkLabel3 = new LinkLabel();
+            linkLabel4 = new LinkLabel();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(99, 53);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(63, 20);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Account";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.Location = new Point(241, 53);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.Size = new Size(74, 20);
+            linkLabel2.TabIndex = 1;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Hardware";
+            linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // linkLabel3
+            // 
+            linkLabel3.AutoSize = true;
+            linkLabel3.Location = new Point(425, 53);
+            linkLabel3.Name = "linkLabel3";
+            linkLabel3.Size = new Size(68, 20);
+            linkLabel3.TabIndex = 2;
+            linkLabel3.TabStop = true;
+            linkLabel3.Text = "Software";
+            linkLabel3.LinkClicked += linkLabel3_LinkClicked;
+            // 
+            // linkLabel4
+            // 
+            linkLabel4.AutoSize = true;
+            linkLabel4.Location = new Point(611, 53);
+            linkLabel4.Name = "linkLabel4";
+            linkLabel4.Size = new Size(44, 20);
+            linkLabel4.TabIndex = 3;
+            linkLabel4.TabStop = true;
+            linkLabel4.Text = "Store";
+            linkLabel4.LinkClicked += linkLabel4_LinkClicked;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(101, 127);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(554, 292);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(richTextBox1);
+            Controls.Add(linkLabel4);
+            Controls.Add(linkLabel3);
+            Controls.Add(linkLabel2);
+            Controls.Add(linkLabel1);
+            Name = "Form4";
+            Text = "Form4";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel3;
+        private LinkLabel linkLabel4;
+        private RichTextBox richTextBox1;
+    }
+}
